5266ebbe0b22d403838ddb211c2ac3325d9e7b4d
abdiwoli <abdiwolih@gmail.com>
2024-05-14 17:21:06 +0300

    task11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -4,4 +4,4 @@
 
 def schools_by_topic(mongo_collection, topic):
     """ return all the list with spesific topic"""
-    return mongo_collection.find({"topic": topic})
+    return mongo_collection.find({"topics": topic})
