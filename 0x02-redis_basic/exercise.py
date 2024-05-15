029425028ecc9b05b2a7f009a0ee8d469620785c
abdiwoli <abdiwolih@gmail.com>
2024-05-15 21:58:58 +0300

    fix

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,6 +2,7 @@
 """ module docs """
 import redis
 import uuid
+from typing import Union
 
 
 class Cache:
@@ -10,7 +11,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data):
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
