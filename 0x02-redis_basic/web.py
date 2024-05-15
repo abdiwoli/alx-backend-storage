1dbe5bb431bf2fc145328be0218c4ec4bf2ee556
abdiwoli <abdiwolih@gmail.com>
2024-05-16 01:00:37 +0300

    done

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -3,7 +3,7 @@
 from datetime import datetime, timedelta
 import requests
 
-# Define a cache dictionary
+
 cache = {}
 cache_expiration = timedelta(seconds=10)
 
@@ -22,6 +22,7 @@ def get_page(url):
 def cache_data(func):
     """ Decorator for caching functionality """
     def wrapper(url):
+        """ wraper function """
         content = get_page(url)
         return content
 
