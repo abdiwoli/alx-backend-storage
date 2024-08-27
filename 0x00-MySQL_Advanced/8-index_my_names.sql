95903afcda1a54b0c49d0e630e4aec8410ca38b0
=abdiwoli <=>
2024-08-27 22:16:13 +0300

    task8

---
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,8 @@
+-- Create an index on the first letter of the name column
+CREATE INDEX idx_name_first ON names (name(1));
+
+-- Verify the index creation
+SHOW INDEX FROM names;
+
+-- Test the index by performing a query
+SELECT COUNT(name) FROM names WHERE name LIKE 'a%';
