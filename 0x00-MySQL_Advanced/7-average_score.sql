a4f39484a19b92ebf6b52506407f63d461b37801
=abdiwoli <=>
2024-08-27 22:11:24 +0300

    task7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -8,7 +8,7 @@ BEGIN
     DECLARE avg_score FLOAT;
 
     -- Calculate the average score for the given user
-    SELECT AVG(score) INTO avg_score
+    SELECT IFNULL(AVG(score), 0) INTO avg_score
     FROM corrections
     WHERE user_id = user_id;
 
